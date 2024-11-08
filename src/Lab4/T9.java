5a7374c1e9f876c6ebcde22f9427583a744006eb
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2024-11-09 00:05:19 +0500

    lab 4 task 9 done

--- src/Lab4/T9.java
+++ src/Lab4/T9.java
@@ -1,4 +1,15 @@
 package Lab4;
-
 public class T9 {
+    public static int missingNumber(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != i){
+                return i;
+            }
+        }return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr= {0,1,2,3,5,6,7};
+
+        System.out.println("The missing Number in series is : "+ missingNumber(arr));
+    }
 }
