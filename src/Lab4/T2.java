7e4797ae9486896cc82eff0a5939d7bc0e3ebbf6
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2024-11-08 19:33:10 +0500

    Task 3 done

--- src/Lab4/T2.java
+++ src/Lab4/T2.java
@@ -1,4 +1,24 @@
 package Lab4;
 
 public class T2 {
+    public static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int[] arrmerge = new int[arr1.length + arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arrmerge[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            arrmerge[arr1.length + i] = arr2[i];
+        }
+        return arrmerge;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = {99,88,77,55};
+        int[] arr2 = {81,91,71,51};
+        int[] mergedArray = mergeArrays(arr1, arr2);
+        System.out.println("Merged Array:");
+        for (int element : mergedArray) {
+            System.out.print(element + " ");
+        }
+    }
 }
--- src/Lab4/T3.java
+++ src/Lab4/T3.java
@@ -1,4 +1,26 @@
 package Lab4;
 
 public class T3 {
+    public static void main(String[] args) {
+        String[] words = {"madam","DATA STRUCTURE" ,"racecar", "level" ,"Muhtashim"};
+        for (String word : words) {
+            if (Palindrome(word)) {
+                System.out.println(word + " is a palindrome.");
+            } else {
+                System.out.println(word + " is not a palindrome.");
+            }
+        }
+    }
+    public static boolean Palindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
