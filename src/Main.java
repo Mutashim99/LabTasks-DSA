e25ef7cfa10cdaec391d80cb51b72076743fefce
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2024-11-08 19:07:53 +0500

    edited Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 321;
-        int b = 11;
-
-        int gcd = findGCD(a, b);
-        System.out.println("The GCD of " + a + " and " + b + " is: " + gcd);
-    }
-    public static int findGCD(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return findGCD(b, a % b);
     }
 }
