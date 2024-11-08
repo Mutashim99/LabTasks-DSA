d7ef9a343f6f4cf35683e0612d3223bd74292e8f
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2024-11-08 20:05:41 +0500

    lab 4 task 6 done

--- src/Lab4/T6.java
+++ src/Lab4/T6.java
@@ -1,5 +1,15 @@
 package Lab4;
 
 public class T6 {
-
+    public static void main(String[] args) {
+        float[] arr = {1f,1.8f,2.8f,9.0f,8.8f,7.7f,6.6f,5.0f,4.4f,3.3f};
+        float sum = 0;
+        float mean = 0;
+        for(float x : arr){
+            sum += x;
+        }
+        mean = sum/arr.length;
+        System.out.println("The Sum is " + sum);
+        System.out.println("The mean is " + mean);
+    }
 }
