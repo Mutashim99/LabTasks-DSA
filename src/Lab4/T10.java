7c0b4c987280ebb89a2926d29dd186ec3778152c
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2024-11-09 00:58:01 +0500

    lab 4 task 10 done

--- src/Lab4/T10.java
+++ src/Lab4/T10.java
@@ -1,4 +1,29 @@
 package Lab4;
+import java.util.Arrays;
 
 public class T10 {
+    public static void zigzag(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (i % 2 == 0) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            } else {
+                if (arr[i] < arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 7, 8, 6, 2, 1};
+        zigzag(arr);
+
+        System.out.println("Zigzag array: " + Arrays.toString(arr));
+    }
 }
--- src/Lab4/T8.java
+++ src/Lab4/T8.java
@@ -1,5 +1,49 @@
 package Lab4;
 
+import java.util.Arrays;
+
+
 public class T8 {
-    
+    public int[][] findCombinations(int[] numbers, int target) {
+        int maxResults = 100;
+        int[][] results = new int[maxResults][numbers.length];
+        int[] currentCombo = new int[numbers.length];
+        int comboCount = 0;
+
+        Arrays.sort(numbers);
+        comboCount = search(numbers, target, 0, 0, currentCombo, results, comboCount);
+
+        int[][] finalResults = new int[comboCount][];
+        for (int i = 0; i < comboCount; i++) {
+            int comboLength = 0;
+            while (comboLength < results[i].length && results[i][comboLength] != 0) {
+                comboLength++;
+            }
+            finalResults[i] = Arrays.copyOf(results[i], comboLength);
+        }
+        return finalResults;
+    }
+
+    public int search(int[] numbers, int target, int start, int comboIndex, int[] currentCombo, int[][] results, int comboCount) {
+        if (target == 0) {
+            System.arraycopy(currentCombo, 0, results[comboCount], 0, comboIndex);
+            return comboCount + 1;
+        }
+        for (int i = start; i < numbers.length; i++) {
+            if (numbers[i] > target) break;
+            currentCombo[comboIndex] = numbers[i];
+            comboCount = search(numbers, target - numbers[i], i + 1, comboIndex + 1, currentCombo, results, comboCount);
+        }
+        return comboCount;
+    }
+
+    public static void main(String[] args) {
+        T8 t = new T8();
+        int[] numbers = {2,4,6,7,8,9};
+        int target = 10;
+
+        int[][] results = t.findCombinations(numbers, target);
+
+        System.out.println(Arrays.deepToString(results));
+    }
 }
