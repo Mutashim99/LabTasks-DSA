b2a9538c9a814196e32ee3805c4b30ec5d480ebc
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2024-11-08 19:40:24 +0500

    task 4 done

--- src/Lab4/T4.java
+++ src/Lab4/T4.java
@@ -1,4 +1,23 @@
 package Lab4;
 
 public class T4 {
+    public static void main(String[] args) {
+        int[] arr = {2,5,3,7,11,6,32,66,16,91,96};
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int arrs:arr) {
+            if (isEvenOrOdd(arrs)) {
+                evenCount++;
+            }
+            oddCount++;
+        }
+        System.out.println("Even Numbers: " + evenCount);
+        System.out.println("Odd Numbers: " + oddCount);
+    }
+    public static boolean isEvenOrOdd(int n) {
+        if(n % 2 == 0){
+            return true;
+        }
+        return false;
+    }
 }
